9115d2f025fc3526a4cbde4437b8793913ca44c3
n4s <s5935512004@phuket.psu.ac.th>
2019-06-24 21:22:00 +0700

    connecting MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+//app
+const app = express()
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log('Database Connected'))
 
 app.get('/', (req, res) => {
     res.send('hello from node')
 })
 
+//routes
 const port = process.env.PORT || 8000
 
 app.listen(port, () => {
