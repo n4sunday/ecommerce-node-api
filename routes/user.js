7dda35b0469b8a77e06046e4fc11a9f389e7b465
n4s <s5935512004@phuket.psu.ac.th>
2019-06-24 22:05:30 +0700

    controllers

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello from Node')
-})
+const {sayHi} = require('../controllers/user')
 
-module.exports = router
+router.get('/', sayHi)
+
+module.exports = router 
