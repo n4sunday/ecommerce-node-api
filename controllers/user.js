750411daa3dd813584b753a298475ff7276070b9
n4s <s5935512004@phuket.psu.ac.th>
2019-06-25 23:50:14 +0700

    friendly error message

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 exports.signup = (req, res) => {
     // console.log('req.body',req.body)
@@ -6,11 +7,11 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                err
+                err: errorHandler(err)
             })
         }
         res.json({
             user
         })
     })
-}
+ }
