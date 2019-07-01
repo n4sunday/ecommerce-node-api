4d3a91188bda4dfc805901890416cefb7f2217d2
n4s <s5935512004@phuket.psu.ac.th>
2019-07-02 00:28:43 +0700

    send product photo

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require('../controllers/product')
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo
+} = require('../controllers/product')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
@@ -14,7 +25,7 @@ router.post('/product/create/:userId',
 )
 router.delete('/product/:productId/:userId',
     requireSignin,
-    isAdmin, 
+    isAdmin,
     isAuth,
     remove
 )
@@ -30,6 +41,7 @@ router.get('/products', list)
 router.get('/products/related/:productId', listRelated)
 router.get('/products/categories', listCategories)
 router.post('/products/by/search', listBySearch)
+router.get('/product/photo/:productId', photo)
 
 router.param('userId', userById)
 router.param('productId', productById)
