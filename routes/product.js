de2cddb2260ccf8edf19e6034c5834ef7a88d200
n4s <s5935512004@phuket.psu.ac.th>
2019-07-24 15:01:13 +0700

    implementtation of search

--- routes/product.js
+++ routes/product.js
@@ -11,7 +11,8 @@ const {
     listRelated,
     listCategories,
     listBySearch,
-    photo
+    photo,
+    listSearch
 } = require('../controllers/product')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
@@ -38,6 +39,7 @@ router.put('/product/:productId/:userId',
 
 
 router.get('/products', list)
+router.get('/products/search',listSearch)
 router.get('/products/related/:productId', listRelated)
 router.get('/products/categories', listCategories)
 router.post('/products/by/search', listBySearch)
